340e2e9de471474e9460abdd2b01de306e0d3aa9
Jcam2346 <jamescampbell@hotmail.com>
2023-12-16 16:04:27 +1100

    Early steps

---
+++ Scanner.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+# Read test data CSV file into a DataFrame
+path = 'path/to/your/file/'
+data = pd.read_csv(path + 'test data.csv')
+
+
