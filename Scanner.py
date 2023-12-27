3367d59fbfe3cb6583ba6a4560421fe8029e5c77
Jcam2346 <jamescampbell@hotmail.com>
2023-12-27 12:51:22 +1100

    Fixed requirements.txt

--- Scanner.py
+++ Scanner.py
@@ -1,9 +1,20 @@
 import pandas as pd
 import numpy as np
-import tensorflow as tf
+# import tensorflow as tf
 
-# Read test data CSV file into a DataFrame
-path = 'path/to/your/file/'
-data = pd.read_csv(path + 'test data.csv')
+# # Read test data CSV file into a DataFrame
+# path = 'path/to/your/file/'
+# data = pd.read_csv(path + 'test data.csv')
 
+from jobspy import scrape_jobs
 
+jobs = scrape_jobs(
+    site_name=["indeed", "glassdoor"],
+    search_term="Data engineer",
+    location="Sydney",
+    results_wanted=2,
+    country_indeed='Australia'  # only needed for indeed / glassdoor
+)
+print(f"Found {len(jobs)} jobs")
+print(jobs.head())
+jobs.to_csv("C:\Users\James\Python Projects\Data-Engineer-Scanner\jobs.csv", index=False) # to_xlsx
