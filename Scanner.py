e7dad2ab8a071cb245042095f1ed03951581995a
Jcam2346 <jamescampbell@hotmail.com>
2023-12-27 17:07:35 +1100

    Acquired test data using JobSpy scraper

--- Scanner.py
+++ Scanner.py
@@ -1,20 +1,26 @@
 import pandas as pd
 import numpy as np
-# import tensorflow as tf
+import os
+import tensorflow as tf
 
-# # Read test data CSV file into a DataFrame
-# path = 'path/to/your/file/'
-# data = pd.read_csv(path + 'test data.csv')
+# Print the current working directory
+print(os.getcwd())
 
 from jobspy import scrape_jobs
 
+# Scrape jobs from indeed and glassdoor
 jobs = scrape_jobs(
     site_name=["indeed", "glassdoor"],
     search_term="Data engineer",
-    location="Sydney",
-    results_wanted=2,
-    country_indeed='Australia'  # only needed for indeed / glassdoor
+    location="Sydney NSW",
+    results_wanted=15,
+    # only needed for indeed / glassdoor
+    country_indeed='Australia',
+    #proxy="" #must be a string in the format "ip_address:port" starting with http:// or https://
 )
 print(f"Found {len(jobs)} jobs")
 print(jobs.head())
-jobs.to_csv("C:\Users\James\Python Projects\Data-Engineer-Scanner\jobs.csv", index=False) # to_xlsx
+
+jobs.to_csv("Data-Engineer-Scanner/jobs.csv", index=False)
+
+#
