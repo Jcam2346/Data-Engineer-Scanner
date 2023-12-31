02cb89f959c2b56eef7d50a9ce25649665084f68
Jcam2346 <jamescampbell@hotmail.com>
2023-12-31 16:34:58 +1100

    Processing Kaggle Data

--- Data Manipulation.py
+++ Data Manipulation.py
@@ -1,7 +1,16 @@
 import pandas as pd
+import os
 
 # Read the CSV file into a pandas dataframe
-df = pd.read_csv('/c:/Users/James/Python Projects/Data-Engineer-Scanner/Kaggle Dataset.csv', encoding='utf-8')
+print(os.getcwd())
+df = pd.read_csv('Kaggle Dataset.csv', encoding='utf-8')
 
 # Print the first 3 rows of the dataframe
-print(df.head(3))
+print(len(df))
+
+# Filter df to include only rows with 'Data' and 'Engineer' in the Job Title column
+new_df = df[df['Job Title'].str.contains('(?i)data') & df['Job Title'].str.contains('(?i)engineer') & ~df['Job Title'].str.contains('(?i)software')]
+
+# Print the number of rows in the new dataframe
+print(len(new_df))
+print(new_df.head(15))
